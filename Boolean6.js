5b249a1a41110737d9c05378775ec193ba8ebd98
EliseyShchipachev <92044109+EliseyShchipachev@users.noreply.github.com>
2022-01-26 20:05:01 +0200

    Add files via upload

---
+++ Boolean6.js
@@ -0,0 +1,10 @@
+let a, b, c;
+a = prompt ("Число")
+b = prompt ("Число")
+c = prompt ("Число")
+if ((a<b)&&(b<c)){
+    alert("true")
+}
+else{
+    alert("false")
+}
---
+++ boolean8.js
@@ -0,0 +1,9 @@
+let a, b;
+a = prompt ("Число")
+b = prompt ("Число")
+if ((a % 2 == 1)&&(b%2 == 1)){
+    alert("true")
+}
+else{
+    alert("false")
+}
